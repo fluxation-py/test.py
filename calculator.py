24e4a1c703dd697539e97132d3d814bab4068cd9
fluxation <fluxationzero@gmail.com>
2026-03-03 16:26:10 -0600

    Implement basic calculator functionality

--- calculator.py
+++ calculator.py
@@ -1,1 +1,19 @@
-print("Hello")
+expression=input("Enter an expression, (+) (-) (*) (/): ")
+num1=float(input("Enter a number: "))
+num2=float(input("Enter another number:"))
+
+if expression=="+":
+    print(num1+num2)
+
+if expression=="-":
+    print(num1-num2)
+
+if expression=="*":
+    print(num1*num2)
+
+if expression=="/":
+    print(num1/num2)
+
+
+else:
+    print("Please enter a valid expression.")
